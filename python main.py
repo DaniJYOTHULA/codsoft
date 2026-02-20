e81c9ae3b3e7b4507351b63da40da212e8247d58
DANI VICTOR <danivictor559@gmail.com>
2026-02-20 21:29:31 +0530

    Add project details and future improvements section
    Added project documentation and future improvement ideas.

---
+++ python main.py
@@ -0,0 +1,25 @@
+
+---
+
+## 🛠️ Technologies Used
+- Python
+- Tkinter (GUI)
+- Random module
+
+---
+
+## 🎯 Future Improvements
+- Difficulty levels
+- Sound effects
+- Animations
+- Multiplayer mode
+- Leaderboard system
+
+---
+
+## 👨‍💻 Author
+Your Name
+
+---
+
+⭐ If you like this project, consider giving it a star!
